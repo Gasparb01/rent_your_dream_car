7bae580eb3461c4389c400b41e3692cd46f16a6e
enzo crivelli <enzo_crivelli97@hotmail.com>
2019-10-28 18:45:24 -0300

    make a navbar

---
+++ app/models/car.rb
@@ -0,0 +1,5 @@
+class Car < ApplicationRecord
+  belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
+  has_many :reservations, dependent: :destroy
+  has_many :users, through: :reservations
+end
