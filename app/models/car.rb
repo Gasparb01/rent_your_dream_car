04328e67ffd31b3f2aa3c0f5b5f054db9f2f8f23
Gaston Lascano <gastonlascano@gmail.com>
2019-10-28 20:10:15 -0300

    added description and images claudinary

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,5 @@
 class Car < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
   CATEGORIES = ["Classic", "Convertible", "Coupe", "Exotic", "Sedan", "SUV"]
   belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
 end
