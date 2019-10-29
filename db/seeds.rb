3bd278298f7c55e56c3055eedb3451f4fad31c84
gaspar baldunciel <Gasparbaldunciel@gmail.com>
2019-10-29 16:52:27 -0300

    i added the delete and update that was wrong, also corrected the seeds adding category. an also in the index i align category brand and description

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,10 +13,11 @@ puts "Starting seeding process"
   user = User.create!(email: Faker::Internet.email, password: 123456)
   puts "Created #{user.email}"
   rand(3..10).times do
+    category = Faker::Vehicle.car_type
     brand = Faker::Vehicle.make
     model = Faker::Vehicle.model(make_of_model: brand)
     image = Unsplash::Photo.search("cars,#{brand},#{model}").first
-    car = Car.new(brand: brand, model: model, year: Faker::Vehicle.year , description: "#{image.description.nil? ? "" : image.description}: The car license of is #{Faker::Vehicle.license_plate}. It is a #{Faker::Vehicle.fuel_type} car, with #{Faker::Vehicle.transmission} transmition. Its original color from factory is #{Faker::Vehicle.color}.")
+    car = Car.new(brand: brand, category: category, model: model, year: Faker::Vehicle.year , description: "#{image.description.nil? ? "" : image.description}: The car license of is #{Faker::Vehicle.license_plate}. It is a #{Faker::Vehicle.fuel_type} car, with #{Faker::Vehicle.transmission} transmition. Its original color from factory is #{Faker::Vehicle.color}.")
     car.owner = user
     car.remote_photo_url = image.urls["regular"]
     car.save!
