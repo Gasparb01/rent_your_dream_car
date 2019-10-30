5a03279216ab088a3c9a12d33554aaa07958e8cc
enzo crivelli <enzo_crivelli97@hotmail.com>
2019-10-30 13:24:09 -0300

    fix add location

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -41,7 +41,7 @@ class CarsController < ApplicationController
   private
 
   def car_params
-    params.require(:car).permit(:model, :year, :brand, :category, :description, :photo)
+    params.require(:car).permit(:model, :year, :brand, :category, :description, :location, :photo)
   end
 
   def set_car
